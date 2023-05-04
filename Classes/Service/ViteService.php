aa38f2cff67217d01f9f58cbe4451f64b3dfc1b0
Simon Praetorius <simon@praetorius.me>
2023-05-04 18:11:25 +0200

    Initial commit

---
+++ Classes/Service/ViteService.php
@@ -0,0 +1,131 @@
+<?php
+declare(strict_types=1);
+namespace Praetorius\ViteAssetCollector\Service;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
+use TYPO3\CMS\Core\Page\AssetCollector;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Praetorius\ViteAssetCollector\Exception\ViteException;
+
+class ViteService
+{
+    const DEFAULT_PORT = 5173;
+
+    public function __construct(
+        protected AssetCollector $assetCollector
+    ) {
+    }
+
+    public function determineDevServer(ServerRequestInterface $request): UriInterface
+    {
+        $vitePort = getenv('VITE_PRIMARY_PORT') ?: self::DEFAULT_PORT;
+        return $request->getUri()->withPath('')->withPort((int) $vitePort);
+    }
+
+    public function addAssetsFromDevServer(
+        UriInterface $devServerUri,
+        string $entry,
+        array $assetOptions = [],
+        array $scriptTagAttributes = []
+    ): void {
+        $scriptTagAttributes = $this->prepareScriptAttributes($scriptTagAttributes);
+        $this->assetCollector->addJavaScript(
+            'vite',
+            (string) $devServerUri->withPath('@vite/client'),
+            ['type' => 'module', ...$scriptTagAttributes],
+            $assetOptions
+        );
+        $this->assetCollector->addJavaScript(
+            "vite:${entry}",
+            (string) $devServerUri->withPath($entry),
+            ['type' => 'module', ...$scriptTagAttributes],
+            $assetOptions
+        );
+    }
+
+    public function addAssetsFromManifest(
+        string $manifestFile,
+        string $entry,
+        bool $addCss = true,
+        array $assetOptions = [],
+        array $scriptTagAttributes = [],
+        array $cssTagAttributes = []
+    ): void {
+        $manifestFile = $this->resolveManifestFile($manifestFile);
+        $manifestDir = dirname($manifestFile) . '/';
+        $manifest = $this->parseManifestFile($manifestFile);
+
+        if (!isset($manifest[$entry]) || !$manifest[$entry]['isEntry']) {
+            throw new ViteException(sprintf(
+                'Invalid vite entry point "%s" in manifest file "%s".',
+                $entry,
+                $manifestFile
+            ), 1683200524);
+        }
+
+        $scriptTagAttributes = $this->prepareScriptAttributes($scriptTagAttributes);
+        $this->assetCollector->addJavaScript(
+            "vite:${entry}",
+            $manifestDir . $manifest[$entry]['file'],
+            ['type' => 'module', ...$scriptTagAttributes],
+            $assetOptions
+        );
+
+        if ($addCss && !empty($manifest[$entry]['css'])) {
+            $cssTagAttributes = $this->prepareCssAttributes($cssTagAttributes);
+            foreach ($manifest[$entry]['css'] as $file) {
+                $this->assetCollector->addStyleSheet(
+                    "vite:${entry}:${file}",
+                    $manifestDir . $file,
+                    $cssTagAttributes,
+                    $assetOptions
+                );
+            }
+        }
+    }
+
+    protected function resolveManifestFile(string $manifestFile): string
+    {
+        $resolvedManifestFile = GeneralUtility::getFileAbsFileName($manifestFile);
+        if ($resolvedManifestFile === '' || !file_exists($resolvedManifestFile)) {
+            throw new ViteException(sprintf(
+                'Vite manifest file "%s" was resolved to "%s" and cannot be opened.',
+                $manifestFile,
+                $resolvedManifestFile
+            ), 1683200522);
+        }
+        return $resolvedManifestFile;
+    }
+
+    protected function parseManifestFile(string $manifestFile): array
+    {
+        $manifest = json_decode(file_get_contents($manifestFile), true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new ViteException(sprintf(
+                'Invalid vite manifest file "%s": %s.',
+                $manifestFile,
+                json_last_error_msg()
+            ), 1683200523);
+        }
+        return $manifest;
+    }
+
+    protected function prepareScriptAttributes(array $attributes): array
+    {
+        foreach (['async', 'defer', 'nomodule'] as $attr) {
+            if ($attributes[$attr] ?? false) {
+                $attributes[$attr] = $attr;
+            }
+        }
+        return $attributes;
+    }
+
+    protected function prepareCssAttributes(array $attributes): array
+    {
+        if ($attributes['disabled'] ?? false) {
+            $attributes['disabled'] = 'disabled';
+        }
+        return $attributes;
+    }
+}
---
+++ README.md
@@ -0,0 +1,115 @@
+# Vite AssetCollector for TYPO3
+
+This TYPO3 extension uses TYPO3's AssetCollector API to embed frontend assets
+generated with [vite](https://vitejs.dev/). This means that you can use
+vite's hot reloading and hot module replacement features (and many others)
+in your TYPO3 project.
+
+## Installation
+
+The extension can be installed via composer:
+
+```sh
+composer req praetorius/vite-asset-collector
+```
+
+## Usage
+
+### Vite Configuration
+
+First, you need to make sure that vite:
+
+* generates a `manifest.json` file and
+* outputs assets to a publicly accessible directory
+
+Example **vite.config.js**:
+
+```js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    publicDir: false,
+    build: {
+        manifest: true,
+        rollupOptions: {
+            input: 'path/to/sitepackage/Resources/Private/JavaScript/Main.js'
+        },
+        outDir: 'path/to/sitepackage/Resources/Public/Vite/',
+    },
+    css: {
+        devSourcemap: true,
+    }
+})
+```
+
+Note that you should not use `resolve(__dirname, ...)` for `input` because the
+value is both a path and an identifier.
+
+### Fluid Usage
+
+Then you can use the included ViewHelper to embed your assets. Note that the
+`entry` value is both a path and an identifier, which is why we cannot
+use `EXT:` here. This also means that this path needs to be consistent between
+your development and your production environment.
+
+Example **Layouts/Default.html**:
+
+```xml
+<html
+    data-namespace-typo3-fluid="true"
+    xmlns:vac="http://typo3.org/ns/Praetorius/ViteAssetCollector/ViewHelpers"
+>
+
+...
+
+<vac:asset.vite
+    manifest="EXT:sitepackage/Resources/Public/Vite/manifest.json"
+    entry="path/to/sitepackage/Resources/Private/JavaScript/Main.js"
+/>
+```
+
+## Configuration
+
+The extension has two configuration options to setup the vite dev server.
+By default, both are set to `auto`, which means:
+
+* Dev server will only be used in `Development` context
+* Dev server uri will be determined automatically for environments with
+[vite-serve for DDEV](https://github.com/torenware/ddev-viteserve) set up
+
+You can adjust both options in your `$TYPO3_CONF_VARS`, for example:
+
+```php
+// Setup vite dev server based on configuration in .env file
+// TYPO3_VITE_DEV_SERVER='https://localhost:1234'
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['vite_asset_collector']['useDevServer'] = (bool) getenv('TYPO3_VITE_DEV_SERVER');
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['vite_asset_collector']['devServerUri'] = (string) getenv('TYPO3_VITE_DEV_SERVER');
+```
+
+## ViewHelper Arguments
+
+* `devTagAttributes` (type: `array`): HTML attributes that should be added to
+script tags that point to the vite dev server
+* `scriptTagAttributes` (type: `array`): HTML attributes that should be added
+to script tags for built JavaScript assets
+* `cssTagAttributes` (type: `array`): HTML attributes that should be added to
+css link tags for built CSS assets
+* `priority` (type: `bool`, default: `false`): Include assets before other assets
+in HTML
+
+Example:
+
+```xml
+<vac:asset.vite
+    manifest="EXT:sitepackage/Resources/Public/Vite/manifest.json"
+    entry="path/to/sitepackage/Resources/Private/JavaScript/Main.js"
+    scriptTagAttributes="{
+        type: 'text/javascript',
+        async: 1
+    }"
+    cssTagAttributes="{
+        media: 'print'
+    }"
+    priority="1"
+/>
+```
