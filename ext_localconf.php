fb67ae87e1a31373dc79c6208e5c9a001f1cba29
Simon Praetorius <simon@praetorius.me>
2023-05-21 12:59:19 +0200

    [TASK] Check for TYPO3 constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend;
 
+defined('TYPO3') or die();
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['viteassetcollector_manifest']
     ??= [];
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['viteassetcollector_manifest']['backend']
