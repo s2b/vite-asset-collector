f48b11f7b2aab158cc63a582c4f5dba3f642f560
Jochen <rothjochen@gmail.com>
2024-02-02 14:25:40 +0100

    [FEATURE] Add v13 support (#47)
    * Add v13 support
    
    * [TASK] Special handling for v13 doctrine dependency in CI
    
    * [TASK] Prevent deprecation warning in functional test
    
    ---------
    
    Co-authored-by: Simon Praetorius <simon@praetorius.me>

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.8.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
         ],
     ],
     'autoload' => [
