e9d774e54eb86a988b5b5880df8d3d28320c9ed6
Simon Praetorius <moin@praetorius.me>
2024-07-29 22:37:42 +0200

    [TASK] Fix CGL issues

--- Build/Testing/UnitTestsBootstrap.php
+++ Build/Testing/UnitTestsBootstrap.php
@@ -26,7 +26,7 @@
  * according script within TYPO3 core's Build/Scripts directory and
  * adapt to extensions needs.
  */
-(static function() {
+(static function () {
     $testbase = new \TYPO3\TestingFramework\Core\Testbase();
 
     // These if's are for core testing (package typo3/cms) only. cms-composer-installer does
