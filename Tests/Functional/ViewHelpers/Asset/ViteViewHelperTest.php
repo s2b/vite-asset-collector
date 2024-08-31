fc27c510721405454b701150cd17040dcd3e34b6
Simon Praetorius <moin@praetorius.me>
2024-08-31 15:51:42 +0200

    [TASK] Use Fluid TemplateView in tests

--- Tests/Functional/ViewHelpers/Asset/ViteViewHelperTest.php
+++ Tests/Functional/ViewHelpers/Asset/ViteViewHelperTest.php
@@ -14,9 +14,9 @@ use TYPO3\CMS\Core\Page\AssetCollector;
 use TYPO3\CMS\Extbase\Mvc\ExtbaseRequestParameters;
 use TYPO3\CMS\Extbase\Mvc\Request;
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
-use TYPO3\CMS\Fluid\View\TemplateView;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\View\TemplateView;
 
 final class ViteViewHelperTest extends FunctionalTestCase
 {
