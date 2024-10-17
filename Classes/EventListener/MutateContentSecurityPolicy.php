1149676f040017df914a647f2243527fbc301bf1
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2024-10-17 13:54:02 +0200

    [FEATURE] Mutate CSP rules to allow vite-server (#77)
    If TYPO3's content security policy is enabled, certain rules need to be loosened
    for the Vite dev server to work properly. Also, Vite reads the current `nonce` value
    from a meta tag in the html `<head>`, which is now provided as well if the dev server
    is active.
    
    This change does not impact the CSP rules on production systems since the special
    rules are only applied if the Vite dev server is in use, which is not the case in production.

---
+++ Classes/EventListener/MutateContentSecurityPolicy.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Praetorius\ViteAssetCollector\EventListener;
+
+use Praetorius\ViteAssetCollector\Service\ViteService;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Event\PolicyMutatedEvent;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
+
+final class MutateContentSecurityPolicy
+{
+    public function __construct(
+        private readonly ViteService $viteService,
+    ) {}
+
+    public function __invoke(PolicyMutatedEvent $event): void
+    {
+        if (!$this->viteService->useDevServer()) {
+            return;
+        }
+
+        $request = $GLOBALS['TYPO3_REQUEST'] ?? new ServerRequest();
+        $viteServerUri = $this->viteService->determineDevServer($request);
+        $uris = [
+            new UriValue((string)$viteServerUri),
+            new UriValue('wss://' . $viteServerUri->getHost() . ':' . $viteServerUri->getPort()),
+        ];
+
+        // Allow viteServer url in CSP
+        $event->getCurrentPolicy()->extend(
+            Directive::ConnectSrc,
+            ...$uris,
+        );
+        $event->getCurrentPolicy()->extend(
+            Directive::ScriptSrc,
+            ...$uris,
+        );
+        $event->getCurrentPolicy()->extend(
+            Directive::StyleSrcElem,
+            ...$uris,
+        );
+        $event->getCurrentPolicy()->extend(
+            Directive::FontSrc,
+            ...$uris,
+        );
+        $event->getCurrentPolicy()->extend(
+            Directive::ImgSrc,
+            ...$uris,
+        );
+    }
+}
---
+++ Classes/Middleware/AddCspNonceMetaTag.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Praetorius\ViteAssetCollector\Middleware;
+
+use Praetorius\ViteAssetCollector\Service\ViteService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Domain\ConsumableString;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+final class AddCspNonceMetaTag implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly ViteService $viteService,
+    ) {}
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $nonceAttribute = $request->getAttribute('nonce');
+        if (!$nonceAttribute instanceof ConsumableString) {
+            return $handler->handle($request);
+        }
+
+        if (!$this->viteService->useDevServer()) {
+            return $handler->handle($request);
+        }
+
+        // Add metatag to <head> to allow vite to consume the current nonce
+        // see: https://de.vitejs.dev/guide/features.html#content-security-policy-csp
+        $nonce = $nonceAttribute->consume();
+        $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+        $pageRenderer->addHeaderData('<meta property="csp-nonce" nonce="' . $nonce . '">');
+
+        return $handler->handle($request);
+    }
+}
