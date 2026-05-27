d0ad21f7b638b181c5f5e5172c3f5faf1b0cd595
Simon Praetorius <moin@praetorius.me>
2026-05-27 16:55:38 +0200

    [TASK] Mark "inlineCss" feature as experimental
    The newly introduced "inlineCss" feature is now documented as
    experimental: Currently, the inlined CSS doesn't reference external
    asset files (such as webfonts or images) with their correct path. Thus,
    it currently only works with basic CSS that doesn't have any
    dependencies to other files.
    
    This cannot reasonably be addressed in v1 of the extension because there
    are several different cases to consider. All require more in-depth
    interpretation of the `manifest.json` file, which with the current
    class structure isn't possible.
    
    You are free to use the now existing flag, but expect that this feature
    will change considerably with v2.

--- Classes/ViewHelpers/AssetViewHelper.php
+++ Classes/ViewHelpers/AssetViewHelper.php
@@ -73,7 +73,7 @@ final class AssetViewHelper extends AbstractViewHelper implements ViewHelperNode
         $this->registerArgument('devTagAttributes', 'array', 'HTML attributes that should be added to script tags that point to the vite dev server', false, []);
         $this->registerArgument('scriptTagAttributes', 'array', 'HTML attributes that should be added to script tags for built JavaScript assets', false, []);
         $this->registerArgument('addCss', 'boolean', 'If set to "false", CSS files associated with the entry point won\'t be added to the asset collector', false, true);
-        $this->registerArgument('inlineCss', 'boolean', 'If set to "true", CSS will be added as inline <style> tag.', false, false);
+        $this->registerArgument('inlineCss', 'boolean', 'If set to "true", CSS will be added as inline <style> tag. Note that this is currently experimental due to missing path rewriting for asset files.', false, false);
         $this->registerArgument('cssTagAttributes', 'array', 'Additional attributes for css link tags.', false, []);
         $this->registerArgument(
             'priority',
